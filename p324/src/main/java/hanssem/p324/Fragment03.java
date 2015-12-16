7dc0d9e08084a8ce5254768c77a880cf31474e01
Donghyun Sohn <sdh01643@gmail.com>
2015-12-16 14:50:41 +0900

    Initial commit

---
+++ p324/src/main/java/hanssem/p324/Fragment03.java
@@ -0,0 +1,17 @@
+package hanssem.p324;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class Fragment03 extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState ) {
+        return inflater.inflate( R.layout.frag03, container, false );
+    }
+
+}
